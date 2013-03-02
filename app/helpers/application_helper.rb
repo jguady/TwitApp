e5731d6c2993f4d5f62fd14ed5c1e0526eddeb35
Jay Guady <eternaldragon54@hotmail.com>
2013-03-01 20:19:57 -0700

    added base title to application helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+
+# Define a title Helper
+	def title
+		base_title = "Ruby on Rails Tutorial Twit App"
+		if @title.nil?
+			base_title
+		else
+			"#{base_title} | #{@title}"
+		end
+	end
 end
